5fb752ef72d0fbfcb2e44593f5e8dd93f7b3bfb9
kaushik94-git <72421015+kaushik94-git@users.noreply.github.com>
2020-11-17 15:24:47 -0500

    Update 1.py

--- Arrays and Strings/1.py
+++ Arrays and Strings/1.py
@@ -1,1 +1,13 @@
+def tie_em_up(a,b,c):
+    out = []
+    for ii in range(0,len(a)):
+        out.append((a[ii],b[ii],c[ii]))
+    return out
 
+
+if  __name__ == '__main__':
+    a = ["John", "Charles", "Mike"]
+    b = ["Jenny", "Christy", "Monica"]
+    c = ['Harish', 'Kaushik', 'Hemant']
+  
+    print(tie_em_up(a,b,c))  
